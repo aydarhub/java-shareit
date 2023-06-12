27738f78a713f39314fde6c718eeb8965f16de39
aydarhub <aydarhub@yandex.ru>
2023-06-13 00:03:53 +0300

    add-bookings / review 2 исправление checkstyle

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.item.dto.ItemWithBookingsResponseDto;
 
 @Getter
 @Setter
